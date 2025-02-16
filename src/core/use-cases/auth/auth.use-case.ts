54a42444311052109e5ce86c6a9fea7879ac03a3
nielitton <nieliton.sousa3040@gmail.com>
2025-02-16 13:49:33 -0300

    fix: login auth not validating

--- src/core/use-cases/auth/auth.use-case.ts
+++ src/core/use-cases/auth/auth.use-case.ts
@@ -1,29 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcryptjs";
 import { BusinessException } from "src/core/exception/businnes-exception";
 import { LoginDto } from "src/core/models/dtos/user-dto";
 import { AuthRepository } from "src/core/repositories/auth/auth.repository";
-import * as bcrypt from "bcryptjs"
 
 @Injectable()
 export class AuthUserUseCase {
-    constructor( 
+    constructor(
         private readonly authRepository: AuthRepository
-    ) {}
+    ) { }
 
     async execute(data: LoginDto) {
         const { email, password } = data
 
-        
         const user = await this.authRepository.findUser(email);
-        
+
         // Essas validações podem ser feitas em middlewares, porém optei por não usar esse método, por serem poucas.
-        if(!user) {
+        if (!user) {
             throw new BusinessException("Email ou senha incorretos", 401)
         }
-        
+
         const validatePassword = await bcrypt.compare(password, user.password)
-        
-        if(!validatePassword) {
+
+        if (!validatePassword) {
             throw new BusinessException("Email ou senha incorretos", 401)
         }
 
